f4df2bd320205b6e93857081f5e90dfd196e0904
Dagon-anchient-god <71090578+Dagon-anchient-god@users.noreply.github.com>
2021-11-30 18:33:22 +0300

    Библиотека с алгоритмом Дейкстры

---
+++ pch1.cpp
@@ -0,0 +1,41 @@
+﻿// pch.cpp: файл исходного кода, соответствующий предварительно скомпилированному заголовочному файлу
+
+#include "pch.h"
+#include <map>
+const int INF = 1000000000;
+
+float shortest_length(const graph& graph, const std::string& src, const std::string& dst)
+{
+
+	std::map<std::string,float> a;
+	std::map<std::string, bool> u;
+	for (auto i : graph)
+	{
+		a.insert({ i.first,INF });
+		a.insert({ i.first,false });
+	}
+	a[src] = 0;
+	for (auto i : graph)
+	{
+		std::string v = "new";
+		weight_map  d;
+		for (auto j : i.second)
+			if (u[j.first] == false && (v == "new" || a[j.first] < a[v]))
+			{
+				v = j.first;
+				d[v] = j.second;
+			}
+
+		if (a[v] == INF)
+			break;
+		u[v] = true;
+
+		for (auto k : d)
+		{
+			std::string to = k.first;
+			float len = k.second;
+			a[to] = min(a[v] + len, a[to]);
+		}
+	}
+	return a[dst];
+}
---
+++ pch1.h
@@ -0,0 +1,30 @@
+﻿// pch.h: это предварительно скомпилированный заголовочный файл.
+// Перечисленные ниже файлы компилируются только один раз, что ускоряет последующие сборки.
+// Это также влияет на работу IntelliSense, включая многие функции просмотра и завершения кода.
+// Однако изменение любого из приведенных здесь файлов между операциями сборки приведет к повторной компиляции всех(!) этих файлов.
+// Не добавляйте сюда файлы, которые планируете часто изменять, так как в этом случае выигрыша в производительности не будет.
+
+#ifndef PCH_H
+#define PCH_H
+
+
+#include <windows.h>
+#include <map>
+#include <string>
+
+#ifdef DEYKSTRA_EXPORTS
+	#define DEYKSTRA_EXPORTS __declspec(dllexport)
+#else
+	#define DEYKSTRA_EXPORTS __declspec(dllimport)
+#endif
+
+typedef std::map<std::string, float> weight_map;    /// название вершины -> вес ребра к этой вершине
+typedef std::map<std::string, weight_map> graph;    /// название вершины -> набор вершин, в которые можно попасть из данной
+
+float DEYKSTRA_EXPORTS shortest_length(const graph& graph, const std::string& src, const std::string& dst);
+
+
+
+#include "framework.h"
+
+#endif PCH_H
