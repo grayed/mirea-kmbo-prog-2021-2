234ca80c54ca8146db91f59603f4577ca90455dd
marksavv <70964357+marksavv@users.noreply.github.com>
2021-11-30 19:03:01 +0300

    Update main.cpp

--- graphs/bellman-ford/main.cpp
+++ graphs/bellman-ford/main.cpp
@@ -1,30 +1,28 @@
-#include <common.h>
+#include "pch.h"
+#include "..\GraphShortPath\common.h"
+#include <map>
+#define INF 1e6
 
-float DLL_EXPORT shortest_length(const graph &graph, const std::string &src, const std::string &dst) {
-    /// TODO
-    return 0;
-}
+float DLL_EXPORT shortest_length(const graph& graph, const std::string& src, const std::string& dst) {
+	auto search = graph.find(src);	//проверяем наличие вершины в графе 
+	auto search2 = graph.find(dst);
+	if (search == graph.end() || search2 == graph.end())
+		return -1;
 
-extern "C" DLL_EXPORT BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
-{
-    switch (fdwReason)
-    {
-        case DLL_PROCESS_ATTACH:
-            // attach to process
-            // return FALSE to fail DLL load
-            break;
-
-        case DLL_PROCESS_DETACH:
-            // detach from process
-            break;
-
-        case DLL_THREAD_ATTACH:
-            // attach to thread
-            break;
-
-        case DLL_THREAD_DETACH:
-            // detach from thread
-            break;
-    }
-    return TRUE; // succesful
+	std::map<std::string, bool> d;
+	for (const auto& g : graph) {
+		d.insert({ g.first, INF });
+	}
+	for (auto i = 0; i < graph.size() - 1; ++i) {
+		for (const auto& g : graph)
+			for (const auto& w : g.second)
+			{
+				if (d[w.first] > d[g.first] + w.second)
+					d[w.first] = d[g.first] + w.second;
+			}
+	}
+	//return 0;
+	if (d[dst] == INF)
+		return -1;
+	return d[dst];
 }
