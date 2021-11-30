ef5c267d91fda6a254c9ab9893a48aaa73ac56f1
Jakos <jakosvadim@gmail.com>
2021-11-30 18:56:52 +0300

    First commit

--- copy-print.cpp
+++ copy-print.cpp
@@ -15,12 +15,17 @@ using namespace std;
 
 template<typename T>
 void copy_array(const T *from, T *to, size_t count) {
-    // TODO
+    for (size_t i = 0; i < count; i++) {
+        to[i] = from[i];
+    }
 }
 
 template<typename T>
 void print_array(const T *parr, size_t count) {
-    // TODO
+    for (size_t i = 0; i < count; i++) {
+        cout << parr[i] << " ";
+    }
+    cout << endl;
 }
 
 int main(int argc, char **argv) {
