e6ff276dbd3b8c8c36bd10a849f484ff0ee5c9ed
Polina Zolotareva <52978966+polin-drom@users.noreply.github.com>
2021-11-30 18:36:14 +0300

    added bellman-ford algorithm

--- graphs/bellman-ford/main.cpp
+++ graphs/bellman-ford/main.cpp
@@ -1,8 +1,22 @@
 #include <common.h>
 
-float DLL_EXPORT shortest_length(const graph &graph, const std::string &src, const std::string &dst) {
-    /// TODO
-    return 0;
+float DLL_EXPORT shortest_length(const graph& graph, const std::string src, const std::string dst)
+{
+    if (graph.find(src) == graph.end() || graph.find(dst) == graph.end())
+        return -1; //если таких вершин нет
+
+    std::map<std::string, float> path;
+    for (const auto& g : graph)
+        path.insert({ g.first, std::numeric_limits<float>::infinity() });
+
+    for (int i = 0; i < graph.size() - 1; ++i) {
+        for (const auto& g : graph)
+            for (const auto& to : g.second)
+                path[to.first] = min(path[to.first], path[g.first] + to.second);
+    }
+
+    if (path[dst] == 1e9) return -1;
+    return path[dst];
 }
 
 extern "C" DLL_EXPORT BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
