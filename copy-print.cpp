a8a484548f487df4694c830e6f87e32b92497eaf
MathPerv <70916051+MathPerv@users.noreply.github.com>
2021-12-07 18:32:09 +0300

    Update copy-print.cpp
    Задание выполнено

--- copy-print.cpp
+++ copy-print.cpp
@@ -1,34 +1,45 @@
-#include <cstdint>
 #include <iostream>
 #include <cstdlib>
-
-// Домашнее задание:
-//
-// 1. Дореализовать copy_array() и print_array()
-//
-// 2. Реализовать функцию поиска минимума в заданном массиве:
-//       const T *where, size_t count, const T &what
-//     Должна возвращать индекс найденного элемента (-1 если не найден,
-//     но 0 для пустого массива).
-
-using namespace std;
+#include <cstdint>
+#include <algorithm>
 
 template<typename T>
-void copy_array(const T *from, T *to, size_t count) {
-    // TODO
+void print_array(const T parr, size_t count)
+{
+	for (size_t i = 0; i < count; i++)
+		std::cout << parr[i] << std::endl;
 }
 
 template<typename T>
-void print_array(const T *parr, size_t count) {
-    // TODO
+void copy_array(const T* from, T* to, size_t count)
+{
+	for (size_t i = 0; i < count; i++)
+		to[i] = from[i];
 }
 
-int main(int argc, char **argv) {
-    int arr[20], arr2[20];
-    for (auto i = 0; i < sizeof(arr)/sizeof(arr[0]); i++)
-        arr[i] = rand();
-    copy_array(arr, arr2, sizeof(arr)/sizeof(arr[0]));
-    print_array(arr, sizeof(arr)/sizeof(arr[0]));
-    print_array(arr2, sizeof(arr2)/sizeof(arr2[0]));
-    return 0;
+// Функция поиска минимума в массиве
+template<typename T>
+T func_min(T* arr, size_t count)
+{
+	if (count == 0)
+		return 0;
+	T min = arr[0];
+	for (size_t i = 1; i < count; i++)
+	{
+		if (arr[i] < min) min = arr[i];
+	}
+	return min;
 }
+
+int main(int argc, char** argv)
+{
+	int arr[20], arr2[20];
+	for (auto i = 0; i < sizeof(arr) / sizeof(arr[0]); i++)
+		arr[i] = rand();
+	copy_array(arr, arr2, sizeof(arr) / sizeof(arr[0]));
+	print_array(arr, sizeof(arr) / sizeof(arr[0]));
+	print_array(arr2, sizeof(arr2) / sizeof(arr2[0]));
+	std::cout << func_min(arr, sizeof(arr) / sizeof(arr[0]));
+	return 0;
+}
+
