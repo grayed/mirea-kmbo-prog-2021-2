23ae33d9e0b56fa137b9d96b5fc2b0a725aeab6a
alexgruzberg_host <sasha@gruzberg.ru>
2021-12-07 13:10:44 +0300

    New Finder + common.h

---
+++ graphs/GraphPathFinder/common.h
@@ -0,0 +1,26 @@
+#pragma once
+#ifndef COMMON_H_INCLUDED
+#define COMMON_H_INCLUDED
+
+#include <windows.h>
+#include <map>
+#include <string>
+
+//#ifdef DIJKSTRA_EXPORTS
+//#BUILD_DLL
+#ifdef BUILD_DLL
+#define DLL_EXPORT __declspec(dllexport)
+#else
+#define DLL_EXPORT __declspec(dllimport) 
+#endif
+
+typedef std::map<std::string, float> weight_map;    /// название вершины -> вес ребра к этой вершине
+typedef std::map<std::string, weight_map> graph;    /// название вершины -> набор вершин, в которые можно попасть из данной
+
+/// std::string (исходная вершина) -> std::string (конечная вершина) -> float (вес ребра)
+
+float DLL_EXPORT shortest_length(const graph& graph, const std::string& src, const std::string& dst);
+
+
+#endif // COMMON_H_INCLUDED
+//обработать в СВОЙСТВА->ПРЕПРОЦЕССОР
