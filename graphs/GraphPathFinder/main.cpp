f48f68c030a57eed93c3a3000b200c302325b60a
vinramil <71972733+vinramil@users.noreply.github.com>
2021-12-24 14:19:37 +0300

    Update main.cpp

--- graphs/GraphPathFinder/main.cpp
+++ graphs/GraphPathFinder/main.cpp
@@ -9,66 +9,47 @@ using namespace std;
 /// вершина -> { вершина -> вес }
 
 
-graph build_graph(std::istream &is) {
-    /// TODO 1
-    /// считываем строчку ребра; берём из неё названия начальной и конечной вершин ребра, а также вес ребра
-    std::string src = ""; /// исходная вершина
-    std::string dst = ""; /// конечная вершина
-    float weight = 0;    /// вес ребра src-dst
-
-    graph graph;
-
-    /// цикл до конца входного потока данных
-
-    /// FIXME лучше через is.get_line() (например, обернув считанную строку в std::stringstream)
-    is >> src >> dst >> weight;
-
-    /// проверяем, существует ли конечная вершина; если нет - добавляем; если да, используем имеющуюся
-    graph.insert(std::pair<std::string, weight_map>(dst, weight_map()));
-
-    /// проверяем, существует ли исходная вершина; если нет - добавляем; если да, используем имеющуюся
-    std::pair<graph::iterator, bool> insres = graph.insert(std::pair<std::string, weight_map>(dst, weight_map()));
-
-    /// добавляем запись про конечную вершину и вес в исходную
-    insres.first->second.insert(std::pair<std::string, float>(dst, weight));
-
-    /// переходим к следующей строке
+graph build_graph(std::istream &is) 
+{
+    graph g;
+    int ver;    //maybe long int?
+    is >> ver;
+    for (auto i = 0; i < ver; i++) 
+    {
+        string st1, st2;
+        float k;
+        is >> st1 >> st2 >> k;
+        g[st1][st2] = k;    //2-dim array
+    }
+    return g;
 }
 
 int main(int argc, char **argv)
 {
-    if (argc < 2) {
+    if (argc < 2) 
+    {
         cerr << "Need at least 2 points to search" << endl;
         return 1;
     }
-
-    /// 1. Считывать описание вершин и рёбер, формируя граф в оперативной памяти
-    ///     считываем построчно с stdin  (std::cin), разбираем строчку, добавляем запись
-    ///     каждая строка - это три значения: исходная вершина, конечная вершина, вес
+    
     auto graph = build_graph(cin);
 
-    /// 2. Декодировать и верифицировать исходную и конечную вершины графа для определения пути
-    ///     два аргумента командной строки
     string src_name(argv[1]);
     string dst_name(argv[2]);
 
     auto src_it = graph.find(src_name);
     auto dst_it = graph.find(dst_name);
 
-    if (src_it == graph.end()) {
-        cerr << "Unknown source: " << src_name << endl;
-        return 1;
+    if (src_it == graph.end()) 
+    {
+        cerr << "Unknown source: " << src_name << endl;     
     }
-    if (dst_it == graph.end()) {
+    if (dst_it == graph.end()) 
+    {
         cerr << "Unknown destination: " << dst_name << endl;
-        return 1;
     }
 
-    /// 3. Вызвать функцию поиска пути
-    ///     в библиотеке
     auto result = shortest_length(graph, src_it->first, dst_it->first);
-
-    /// 4. Вывести результат
     cout << result << endl;
     return 0;
 }
