cee9fab26e13cba1f2bbff9db0e7d27bb47fb339
vinramil <71972733+vinramil@users.noreply.github.com>
2021-12-24 14:25:07 +0300

    Update main.cpp

--- graphs/bellman-ford/main.cpp
+++ graphs/bellman-ford/main.cpp
@@ -1,30 +1,42 @@
-#include <common.h>
+#include <algorithm>
+#include <limits>
+#include <map>
+#include <set>
+#include <vector>
+#include "..\\Algorithms_on_Graphs\common.h"
 
-float DLL_EXPORT shortest_length(const graph &graph, const std::string &src, const std::string &dst) {
-    /// TODO
+struct Edges
+{
+    std::string e1, e2;
+    float weight;
+};
+
+float DLL_EXPORT shortest_length(const graph &graph, const std::string &src, const std::string &dst) 
+{
+    std::map<std::string, float> labs;
+    std::vector<Edges> tovisit;
+    for (const auto& pass : graph)
+    {
+        for (const auto& t : pass.second)
+        {
+            tovisit.push_back({ pass.first, t.first, t.second });   //rec
+        }
+        if (pass.first == src)
+            labs[pass.first] = 0;
+        else
+            labs[pass.first] = std::numeric_limits<float>::infinity();  //inf labels
+    }
+    for (auto i = 0; i < graph.size() - 1; i++)
+    {
+        for (auto j = 0; j < tovisit.size(); i++)
+        {
+            std::string start = tovisit[j].e1;
+            std::string finish = tovisit[j].e2;
+            float weight = tovisit[j].weight;
+            labs[finish] = min(labs[finish], labs[start] + weight);
+        }
+    }
     return 0;
 }
 
-extern "C" DLL_EXPORT BOOL APIENTRY DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
-{
-    switch (fdwReason)
-    {
-        case DLL_PROCESS_ATTACH:
-            // attach to process
-            // return FALSE to fail DLL load
-            break;
 
-        case DLL_PROCESS_DETACH:
-            // detach from process
-            break;
-
-        case DLL_THREAD_ATTACH:
-            // attach to thread
-            break;
-
-        case DLL_THREAD_DETACH:
-            // detach from thread
-            break;
-    }
-    return TRUE; // succesful
-}
