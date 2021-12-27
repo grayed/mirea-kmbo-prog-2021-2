3b71fc4b4cb759b9a762c37578af59231e324fc9
mcbaragoz <72667321+mcbaragoz@users.noreply.github.com>
2021-12-27 15:09:00 +0300

    Update copy-print.cpp
    Функции копирования и вывода массива, а также поиск минимума

--- copy-print.cpp
+++ copy-print.cpp
@@ -1,26 +1,33 @@
-#include <cstdint>
-#include <iostream>
-#include <cstdlib>
-
-// Домашнее задание:
-//
-// 1. Дореализовать copy_array() и print_array()
-//
-// 2. Реализовать функцию поиска минимума в заданном массиве:
-//       const T *where, size_t count, const T &what
-//     Должна возвращать индекс найденного элемента (-1 если не найден,
-//     но 0 для пустого массива).
-
-using namespace std;
-
 template<typename T>
 void copy_array(const T *from, T *to, size_t count) {
     // TODO
+    for (size_t i = 0; i < count; i++)
+		to[i] = from[i];
 }
 
 template<typename T>
 void print_array(const T *parr, size_t count) {
     // TODO
+    for (size_t i = 0; i < count; i++)
+		cout << parr[i] << endl;
+}
+
+template<typename T>
+size_t findMin(const T* parr, size_t count)
+{
+	size_t min = 0;
+	if (count == 0)
+		return min;
+	T elem = parr[0];
+	for (size_t i = 1; i < count; i++)
+	{
+		if (parr[i] < elem)
+		{
+			elem = parr[i];
+			min = i;
+		}
+	}
+	return min;
 }
 
 int main(int argc, char **argv) {
