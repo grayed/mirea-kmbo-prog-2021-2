4b5fbf3cdca49c320427f064531dcbc71f1ec09f
No Name <49948225+playking@users.noreply.github.com>
2022-05-26 16:52:19 +0300

    Add files via upload

--- copy-print.cpp
+++ copy-print.cpp
@@ -14,13 +14,33 @@
 using namespace std;
 
 template<typename T>
-void copy_array(const T *from, T *to, size_t count) {
-    // TODO
+void copy_array(const T* from, T* to, size_t count) {
+    for (size_t i = 0; i < count; i++)
+        to[i] = from[i];
 }
 
 template<typename T>
-void print_array(const T *parr, size_t count) {
-    // TODO
+void print_array(const T* parr, size_t count) {
+    for (size_t i = 0; i < count; i++)
+        cout << parr[i] << endl;
+}
+
+template<typename T>
+size_t find_min(const T* parr, size_t count)
+{
+    size_t min = 0;
+    if (count == 0)
+        return min;
+    T min_el = parr[0];
+    for (size_t i = 1; i < count; i++)
+    {
+        if (parr[i] < min_el)
+        {
+            min_el = parr[i];
+            min = i;
+        }
+    }
+    return min;
 }
 
 int main(int argc, char **argv) {
@@ -30,5 +50,7 @@ int main(int argc, char **argv) {
     copy_array(arr, arr2, sizeof(arr)/sizeof(arr[0]));
     print_array(arr, sizeof(arr)/sizeof(arr[0]));
     print_array(arr2, sizeof(arr2)/sizeof(arr2[0]));
+    find_min(arr, sizeof(arr)/sizeof(arr[0]));
+    find_min(arr2, sizeof(arr2)/sizeof(arr2[0]));
     return 0;
 }
